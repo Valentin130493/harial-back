07f33e33516c4bac2cc8c8aa47b46b6c0a7b6604
Valentin130493 <gerasimenko130493@gmail.com>
2022-11-17 13:19:31 +0200

    add env file to project and create new variable server

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 
 const app = express()
 
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
