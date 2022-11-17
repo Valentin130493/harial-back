32b5c23c5c2a56b396cbc1e099b5da75d3c41b7f
Valentin130493 <gerasimenko130493@gmail.com>
2022-11-17 13:50:04 +0200

    add router to project

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv');
 dotenv.config();
-
-
+const bodyParser = require('body-parser')
+const router = require('./router/index.js')
 const PORT = process.env.PORT || 8080
 
 
 const app = express()
-
+app.use(express.json())
+app.use(bodyParser.json({limit: '50mb'}))
+app.use('/', router)
 
 const server = app.listen(PORT, () => {
-
     console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
