221a1e064d206d2fcd83ba7bbb62c632955a4e89
Valentin130493 <gerasimenko130493@gmail.com>
2022-11-17 14:48:33 +0200

    connect to db

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 const bodyParser = require('body-parser')
 const router = require('./router/index.js')
+const mongoose = require('mongoose')
+
 const PORT = process.env.PORT || 8080
+const URL = process.env.URL
 
 
 const app = express()
@@ -11,6 +14,13 @@ app.use(express.json())
 app.use(bodyParser.json({limit: '50mb'}))
 app.use('/', router)
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, async () => {
+    try {
+        await mongoose.connect(`${URL}`)
+        console.log('database is connected')
+    } catch (err) {
+        console.log(err, "database error")
+    }
+
     console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
