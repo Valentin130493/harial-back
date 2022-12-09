6e40bdf702531db24b90bf00728ec6ac58313e20
Valentin130493 <gerasimenko130493@gmail.com>
2022-12-09 15:17:19 +0200

    add response to findDoc

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -5,6 +5,12 @@ const findDoc = async (req, res) => {
     try {
         const doc = await DocsModel.find({number: req.body.number})
 
+        const {status} = doc
+
+        res.status(200).json({
+            app_status: status
+        })
+
     } catch (err) {
         console.log(err)
         res.status(500).json({
