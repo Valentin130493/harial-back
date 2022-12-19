c6bbd1f19111de7238e0af08c248c4d20ecf5fd9
Valentin130493 <gerasimenko130493@gmail.com>
2022-12-19 14:43:03 +0200

    add change status project

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ const Router = require('express').Router
 const router = new Router();
 const multer = require('multer')
 const upload = multer()
+const fs = require('fs')
 
 const FormController = require("../controllers/form/index.js")
 const AdminController = require("../controllers/admin/index.js")
@@ -12,15 +13,18 @@ const StatusController = require("../controllers/status/index.js")
 
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
+        const folder = `1287`
         cb(null, "uploads")
     }, filename: (_, file, cb) => {
         cb(null, file.originalname)
     },
 })
 
-const uploadFiles = multer({storage, limits: {
+const uploadFiles = multer({
+    storage, limits: {
         fileSize: 1024 * 1024 * 10
-    }})
+    }
+})
 
 router.post('/form', upload.none(), FormController.formData)
 router.get('/doc', FormController.findByNumber)
@@ -31,8 +35,8 @@ router.post("/user/login", upload.none(), AuthController.login)
 
 router.get("/status", StatusController.getAllStatus)
 
-router.post("admin/changeStatus", AdminController.changeAppStatus)
-router.post("/admin/upload", uploadFiles.fields, AdminController.uploadsFiles)
+router.post("/admin/changeStatus", AdminController.changeAppStatus)
+router.post("/admin/upload", uploadFiles.any(), AdminController.uploadsFiles)
 
 router.post("/user/search", upload.none(), UserController.findDoc)
 
