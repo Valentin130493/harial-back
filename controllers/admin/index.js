5e3ab5321288eae7ee278ce4f295fe8748b06122
Valentin130493 <gerasimenko130493@gmail.com>
2022-12-21 20:12:00 +0200

    change update project function

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -3,14 +3,14 @@ const fs = require("fs")
 
 const updateProject = async (req, res) => {
     const {number} = req.params
+    console.log(req.body.files)
     try {
         const data = req.files.map((item) => item.path)
-        console.log(data)
-        const project = await ProjectModel.findOneAndUpdate({project_number: Number(number)}, {
+        await ProjectModel.findOneAndUpdate({project_number: Number(number)}, {
             docs: data,
             status: req.body.status
         })
-
+        const project = await ProjectModel.findOne({project_number: Number(number)})
         console.log(project)
         res.status(200).send(project)
     } catch (err) {
@@ -22,7 +22,6 @@ const updateProject = async (req, res) => {
 const removeFiles = async (req, res) => {
     const files = req.body
     const {number} = req.params
-    console.log(files)
 
     await files.forEach((file) => {
         fs.unlink(`${file}`, (err) => {
